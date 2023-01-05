15743aca3f2d5c48f605898bc145e63c233d4f36
aledale <ale.salas271102@gmail.com>
2023-01-05 19:53:56 -0300

    finish cryptocurrency

---
+++ src/components/News.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
+import moment from 'moment'
+import {Link} from 'react-router-dom'
+
+const News = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default News
---
+++ src/services/cryptoApi.ts
@@ -0,0 +1,26 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import axios from 'axios'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '411cb2b1c9mshe9441ecd506f461p163e95jsnbf3948d93d55',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+  }
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+//no hace falta
+const createRequest = (url:string) => ({url,headers:cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl,headers:cryptoApiHeaders}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            //que se le agrega al final a la url
+            query:(count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+})
+
+//te crea metodos
+export const {useGetCryptosQuery} = cryptoApi
